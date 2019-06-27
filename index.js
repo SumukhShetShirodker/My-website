b8152f22f9d522641d580257a990dd2afd89d0bc
SumukhShirodker <sumukhshirodker@gmail.com>
2019-06-27 18:38:58 +0530

    Hello

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
 const path = require("path")
 const PORT=process.env.PORT || 7500
+
+//multer
+
+const storage = multer.diskStorage({
+  destination: './src/uploads/',
+  filename: function(req,file,cb){
+    cb(null, file.feildname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+
+const upload = multer({
+  storage : storage
+}).single('myImage');
+
+// main code
 app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname + '/src')))
 //main code
 app.get('/',(req,res)=>{
   res.render('index.ejs')
-})
-app.get('/login',(req,res)=>{
-  res.render('login.ejs')
 })
 //listenig ports
 app.listen(PORT,()=>{
